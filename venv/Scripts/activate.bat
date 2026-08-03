92c93c7c536f9cb4d2762ee6b81896ab21194d08
Daniel Rodionov <danilrodionov8665@gmail.com>
2026-08-04 01:14:19 +0300

    Gitignore

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Daniel\PycharmProjects\Manager_of_recipes\venv
+set VIRTUAL_ENV=C:\Users\Daniel\PycharmProjects\from-zero-to-junior-test\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
